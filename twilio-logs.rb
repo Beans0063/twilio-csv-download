cadd76dd18e2dd711a35a5abec27ee7f7ed1c076
Beans0063 <beans@Beanserver.net>
2011-04-22 12:32:53 -0700

    cleanup structure

--- twilio-logs.rb
+++ twilio-logs.rb
@@ -1,8 +1,8 @@
-#!/usr/bin/env ruby
+# Sinatra app that gathers voice and sms log data from Twilio and exports as csv
 require 'sinatra'
 require 'erb'
 require 'twiliolib'
-require 'twiliolib-monkeypatch'
+require 'twiliolib-patch'
 require 'nokogiri'
 require "fastercsv"
 
@@ -10,13 +10,21 @@ API_VERSION = '2010-04-01'
 PAGE_SIZE=500
 
 def get_calls(starttime,endtime,pagesize,page)
-  #make a REST call toi Twilio
+  #make a REST call to Twilio Calls resource
   account = Twilio::RestAccount.new(params[:account_sid], params[:auth_token])
   d = { "StartTime>" => starttime,  "EndTime<" => endtime,"PageSize" => pagesize,"Page" => page}
   resp = account.request("/#{API_VERSION}/Accounts/#{params[:account_sid]}/Calls", 'GET', d)
 end
 
+def get_sms(starttime,endtime,pagesize,page)
+  #make a REST call to Twilio SMS/Messages resource
+  account = Twilio::RestAccount.new(params[:account_sid], params[:auth_token])
+  d = { "DateSent>" => starttime,  "DateSent<" => endtime,"PageSize" => pagesize,"Page" => page}
+  resp = account.request("/#{API_VERSION}/Accounts/#{params[:account_sid]}/SMS/Messages", 'GET', d)
+end
+
 def parse_call_record(node)
+  #parse calls xml result and return as a csv friendly array
   call={}
   node.children.each do |child|
     call[child.name]=child.content
@@ -27,21 +35,32 @@ def parse_call_record(node)
   [call["From"],call["To"],call["StartTime"],call["EndTime"],call_mins,price_per_min,call["Price"].to_f.abs,call["Status"],call["Sid"]]
 end
 
+
+def parse_sms_record(node)
+  #parse sms xml result and return as a csv friendly array
+  sms={}
+  node.children.each do |child|
+    sms[child.name]=child.content
+  end
+  [sms["From"],sms["To"],sms["Body"],sms["DateSent"],sms["Price"].to_f.abs,sms["Status"],sms["Sid"]]
+end
+
 def csv_output
-  #fetch initial page
-  resp = get_calls(params[:start_date],params[:end_date],PAGE_SIZE,0)
+  #generate csv export for calls
+  resp = get_calls(params[:start_date],params[:end_date],PAGE_SIZE,0) #fetch initial page of results
   resp.error! unless resp.kind_of? Net::HTTPSuccess
   doc  = Nokogiri::XML(resp.body)
   numpages=((doc.xpath("//Calls").first.attributes["numpages"].value).to_i)-1
+  return "Result too large, try a smaller date range" if numpages > 30
   puts "#{numpages} pages to fetch"
   csv_string = FasterCSV.generate do |csv|
-    csv << ["From","To","Call Start Time","Call End Time","Call Duration (minutes)","Call Price per Minute","Total Call Cost","Call Status","Call SID"]
-    #first page of calls
+    csv << ["From","To","Call Start Time","Call End Time","Call Duration (minutes)","Call Price per Minute","Total Call Cost","Call Status","Call SID"] #header row
+    #output page of calls
     calls=doc.xpath("//Call")
     calls.each do |call|
        csv << parse_call_record(call)
     end
-    (1..numpages).each do |page|
+    (1..numpages).each do |page| #iterate over remaining pages
       puts "fetching #{page}/#{numpages}..."
       resp = get_calls(params[:start_date],params[:end_date],PAGE_SIZE,page)
       resp.error! unless resp.kind_of? Net::HTTPSuccess
@@ -49,6 +68,36 @@ def csv_output
       calls=pagedoc.xpath("//Call")
       calls.each do |call|
          csv << parse_call_record(call)
+      end
+    end
+	
+  end
+  csv_string
+end
+
+def sms_csv_output
+  #generate csv export for sms messages
+  resp = get_sms(params[:start_date],params[:end_date],PAGE_SIZE,0)   #fetch initial page
+  resp.error! unless resp.kind_of? Net::HTTPSuccess
+  doc  = Nokogiri::XML(resp.body)
+  numpages=((doc.xpath("//SMSMessages").first.attributes["numpages"].value).to_i)-1
+  return "Result too large, try a smaller date range" if numpages > 30
+  puts "#{numpages} pages to fetch"
+  csv_string = FasterCSV.generate do |csv|
+    csv << ["From","To","Body","Semt Time","Price","Status","SMS SID"] #csv header row
+    #output page of calls
+    calls=doc.xpath("//SMSMessage")
+    calls.each do |sms|
+       csv << parse_sms_record(sms)
+    end
+    (1..numpages).each do |page| #iterate over remaining pages
+      puts "fetching #{page}/#{numpages}..."
+      resp = get_sms(params[:start_date],params[:end_date],PAGE_SIZE,page)
+      resp.error! unless resp.kind_of? Net::HTTPSuccess
+      pagedoc  = Nokogiri::XML(resp.body)
+      calls=pagedoc.xpath("//SMSMessage")
+      calls.each do |sms|
+         csv << parse_sms_record(sms)
       end
     end
 	
@@ -68,4 +117,12 @@ post '/' do
     "error validating parameters, please ensure account and auth token are correct and dates are in the format of YYYY-MM-DD and try again."
   end
 end
- 
+
+post '/sms' do
+  begin
+    headers "Content-Disposition" => "attachment;filename=report.csv", "Content-Type" => "application/octet-stream"
+    sms_csv_output
+  rescue
+    "error validating parameters, please ensure account and auth token are correct and dates are in the format of YYYY-MM-DD and try again."
+  end
+end
---
+++ twiliolib-patch.rb
@@ -0,0 +1,32 @@
+#monkey patch to encode url to allow for greater than < or less than > in url
+#otherwise invalid url format is thrown
+module Twilio
+  class RestAccount
+      def fetch(url, params, method=nil)
+        if method && method == 'GET'
+          url = build_get_uri(url, params)
+        end
+        #monkey patch here - must encode url to allow for greater than < or less than > in url
+        uri = URI.parse(URI.encode(url))
+        puts url
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        if method && method == 'GET'
+          req = Net::HTTP::Get.new(uri.request_uri)
+        elsif method && method == 'DELETE'
+          req = Net::HTTP::Delete.new(uri.request_uri)
+        elsif method && method == 'PUT'
+          req = Net::HTTP::Put.new(uri.request_uri)
+          req.set_form_data(params)
+        else
+          req = Net::HTTP::Post.new(uri.request_uri)
+          req.set_form_data(params)
+        end
+        req.basic_auth(@id, @token)
+
+        return http.request(req)
+      end
+    end
+end
