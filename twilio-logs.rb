45246e259ac44255a08517e968da22ae06b860f4
Beans0063 <beans@Beanserver.net>
2011-04-26 00:39:57 -0700

    Combine call and sms csv generation into a single function

--- twilio-logs.rb
+++ twilio-logs.rb
@@ -8,23 +8,25 @@ require "fastercsv"
 
 API_VERSION = '2010-04-01'
 PAGE_SIZE=500
+CALL_CSV_HEADERS=["From","To","Call Start Time","Call End Time","Call Duration (minutes)","Call Price per Minute","Total Call Cost","Call Status","Call SID"] #header row
+SMS_CSV_HEADERS=["From","To","Body","Semt Time","Price","Status","SMS SID"] #csv header row
 
 def get_calls(starttime,endtime,pagesize,page)
-  #make a REST call to Twilio Calls resource
+  #make a REST call to Twilio Calls List resource http://www.twilio.com/docs/api/rest/call#list
   account = Twilio::RestAccount.new(params[:account_sid], params[:auth_token])
   d = { "StartTime>" => starttime,  "EndTime<" => endtime,"PageSize" => pagesize,"Page" => page}
   resp = account.request("/#{API_VERSION}/Accounts/#{params[:account_sid]}/Calls", 'GET', d)
 end
 
 def get_sms(starttime,endtime,pagesize,page)
-  #make a REST call to Twilio SMS/Messages resource
+  #make a REST call to Twilio SMS Messages List resource http://www.twilio.com/docs/api/rest/sms#list
   account = Twilio::RestAccount.new(params[:account_sid], params[:auth_token])
   d = { "DateSent>" => starttime,  "DateSent<" => endtime,"PageSize" => pagesize,"Page" => page}
   resp = account.request("/#{API_VERSION}/Accounts/#{params[:account_sid]}/SMS/Messages", 'GET', d)
 end
 
 def parse_call_record(node)
-  #parse calls xml result and return as a csv friendly array
+  #parses call xml into a csv friendly array
   call={}
   node.children.each do |child|
     call[child.name]=child.content
@@ -35,9 +37,8 @@ def parse_call_record(node)
   [call["From"],call["To"],call["StartTime"],call["EndTime"],call_mins,price_per_min,call["Price"].to_f.abs,call["Status"],call["Sid"]]
 end
 
-
 def parse_sms_record(node)
-  #parse sms xml result and return as a csv friendly array
+  #parses sms xml into a csv friendly array
   sms={}
   node.children.each do |child|
     sms[child.name]=child.content
@@ -45,59 +46,55 @@ def parse_sms_record(node)
   [sms["From"],sms["To"],sms["Body"],sms["DateSent"],sms["Price"].to_f.abs,sms["Status"],sms["Sid"]]
 end
 
-def csv_output
-  #generate csv export for calls
-  resp = get_calls(params[:start_date],params[:end_date],PAGE_SIZE,0) #fetch initial page of results
+def csv_output(resource)
+  #generate csv output
+
+  if resource=="calls"
+    container_xpath="//Calls"
+    resource_xpath="//Call"
+    csv_headers=CALL_CSV_HEADERS
+    resp = get_calls(params[:start_date],params[:end_date],PAGE_SIZE,0) #fetch initial page of results
+  elsif resource=="sms"
+    container_xpath="//SMSMessages"
+    resource_xpath="//SMSMessage"
+    csv_headers=SMS_CSV_HEADERS
+    resp = get_sms(params[:start_date],params[:end_date],PAGE_SIZE,0)    #fetch initial page of results
+  end
+  
   resp.error! unless resp.kind_of? Net::HTTPSuccess
   doc  = Nokogiri::XML(resp.body)
-  numpages=((doc.xpath("//Calls").first.attributes["numpages"].value).to_i)-1
+  numpages=((doc.xpath(container_xpath).first.attributes["numpages"].value).to_i)-1
   return "Result too large, try a smaller date range" if numpages > 30
   puts "#{numpages} pages to fetch"
+
+  #construct csv output
   csv_string = FasterCSV.generate do |csv|
-    csv << ["From","To","Call Start Time","Call End Time","Call Duration (minutes)","Call Price per Minute","Total Call Cost","Call Status","Call SID"] #header row
+    csv << csv_headers #header row
     #output page of calls
-    calls=doc.xpath("//Call")
+    calls=doc.xpath(resource_xpath)
     calls.each do |call|
-       csv << parse_call_record(call)
-    end
-    (1..numpages).each do |page| #iterate over remaining pages
-      puts "fetching #{page}/#{numpages}..."
-      resp = get_calls(params[:start_date],params[:end_date],PAGE_SIZE,page)
-      resp.error! unless resp.kind_of? Net::HTTPSuccess
-      pagedoc  = Nokogiri::XML(resp.body)
-      calls=pagedoc.xpath("//Call")
-      calls.each do |call|
-         csv << parse_call_record(call)
+      if resource=="calls"
+        csv << parse_call_record(call)
+      else
+        csv << parse_sms_record(call)
       end
     end
-	
-  end
-  csv_string
-end
-
-def sms_csv_output
-  #generate csv export for sms messages
-  resp = get_sms(params[:start_date],params[:end_date],PAGE_SIZE,0)   #fetch initial page
-  resp.error! unless resp.kind_of? Net::HTTPSuccess
-  doc  = Nokogiri::XML(resp.body)
-  numpages=((doc.xpath("//SMSMessages").first.attributes["numpages"].value).to_i)-1
-  return "Result too large, try a smaller date range" if numpages > 30
-  puts "#{numpages} pages to fetch"
-  csv_string = FasterCSV.generate do |csv|
-    csv << ["From","To","Body","Semt Time","Price","Status","SMS SID"] #csv header row
-    #output page of calls
-    calls=doc.xpath("//SMSMessage")
-    calls.each do |sms|
-       csv << parse_sms_record(sms)
-    end
     (1..numpages).each do |page| #iterate over remaining pages
       puts "fetching #{page}/#{numpages}..."
-      resp = get_sms(params[:start_date],params[:end_date],PAGE_SIZE,page)
+       if resource=="calls"
+         resp = get_calls(params[:start_date],params[:end_date],PAGE_SIZE,page)
+       else
+         resp = get_sms(params[:start_date],params[:end_date],PAGE_SIZE,page)
+       end
       resp.error! unless resp.kind_of? Net::HTTPSuccess
       pagedoc  = Nokogiri::XML(resp.body)
-      calls=pagedoc.xpath("//SMSMessage")
-      calls.each do |sms|
-         csv << parse_sms_record(sms)
+      calls=pagedoc.xpath(resource_xpath)
+      calls.each do |call|
+         if resource=="calls"
+            csv << parse_call_record(call)
+          else
+            csv << parse_sms_record(call)
+          end
       end
     end
 	
@@ -112,7 +109,7 @@ end
 post '/' do
   begin
     headers "Content-Disposition" => "attachment;filename=report.csv", "Content-Type" => "application/octet-stream"
-    csv_output
+    csv_output("calls")
   rescue
     "error validating parameters, please ensure account and auth token are correct and dates are in the format of YYYY-MM-DD and try again."
   end
@@ -121,7 +118,7 @@ end
 post '/sms' do
   begin
     headers "Content-Disposition" => "attachment;filename=report.csv", "Content-Type" => "application/octet-stream"
-    sms_csv_output
+    csv_output("sms")
   rescue
     "error validating parameters, please ensure account and auth token are correct and dates are in the format of YYYY-MM-DD and try again."
   end
